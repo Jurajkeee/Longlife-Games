e7d781e5475984a1d2e3e50486f26d7edddf5859
Mobot <po4tajurajkeee@gmail.com>
2019-02-12 15:03:47 +0200

    Initial commit

---
+++ LonglifeGames/LonglifeGames/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace LonglifeGames.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Longlife Games";
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+        public ActionResult Games()
+        {
+            ViewBag.Title = "Games";
+            return View();
+        }
+        public ActionResult Tools()
+        {
+            ViewBag.Title = "Tools";
+            return View();
+        }
+        public ActionResult Team()
+        {
+            ViewBag.Title = "Team";
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            ViewBag.Title = "Blog";
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            ViewBag.Title = "Contact us";
+            return View();
+        }
+        public ActionResult Article()
+        {
+            ViewBag.Title = "Article";
+            return View();
+        }
+        public ActionResult ToolDescription()
+        {
+            ViewBag.Title = "Description";
+            return View();
+        }
+    }
+}
